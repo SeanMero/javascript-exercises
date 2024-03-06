95b12b5713386b82e7a74cfc23ef5bd3d78b1763
SeanMero <MeroFromVero@gmail.com>
2024-03-06 15:45:57 +0800

    Add solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let bigString = ''
+    if (number < 0) {
+        return 'ERROR'
+    }
+    else {   
+        for (i = 1; i < number + 1; i++) {
+            bigString = bigString + string
+        }
+    }    
+    return bigString
 };
 
 // Do not edit below this line
