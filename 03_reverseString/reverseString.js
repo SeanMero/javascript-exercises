96b169f5c6dfe5794fe7b24503480236137bda8c
SeanMero <MeroFromVero@gmail.com>
2024-03-06 16:08:25 +0800

    Add solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let splitText = text.split("");
+    let length = text.length;
+    let reverseArray = []
+    for (i = 1; i <= length; i++) {
+        reverseArray.push(splitText.at(-i))
+    }
+    let reverseText = reverseArray.join("")
+    return reverseText
 };
 
 // Do not edit below this line
