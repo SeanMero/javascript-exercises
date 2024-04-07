fbe0d7ab56ff572026473b21e5e83e26b37ab3e4
SeanMero <MeroFromVero@gmail.com>
2024-04-07 16:34:29 +0800

    Add solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let answer = 0;
+  for (let i in arr) {
+    answer += arr[i];
+  }
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let answer = 1;
+  for (let i in arr) {
+    answer *= arr[i];
+  };
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let answer = 1;
+  for (let i = a; i >= 0; i--) {
+    if (i === 0) {
+    continue
+    } else {
+      answer *= i};
+  };
+  return answer;  
 };
 
 // Do not edit below this line
