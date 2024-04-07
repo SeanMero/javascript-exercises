f88f35985cc4603372f9cf1ef5cb2b1ad8269f2a
SeanMero <MeroFromVero@gmail.com>
2024-04-07 20:53:18 +0800

    Add partial solution (in order to continue work from home)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+function removePunctuation(text) {
+    const punctuation = /[\.,?!]/g;
+    let newText = text.replace(punctuation, "");
+    return newText;
+  }
+  
 
+const palindromes = function(str) {
+    const cleaned = removePunctuation(str);
+    const old = cleaned.split('');
+    //console.log(old);
+    let newArr = [];
+    //console.log(newArr);
+    for (let i = old.length -1; i >= 0; i--) {
+        newArr.push(old[i]);
+        //console.log(newArr);
+    }
+    let answer = newArr.join('');
+    //console.log(answer);
+    //console.log(str);
+    return answer === cleaned;
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
