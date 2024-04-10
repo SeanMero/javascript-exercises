d7b9e2505acef9e948231fb808c1fd46a9aa41e8
SeanMero <MeroFromVero@gmail.com>
2024-04-10 15:23:28 +0800

    Add finished solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,27 +1,30 @@
 function removePunctuation(text) {
     const punctuation = /[\.,?!]/g;
     let newText = text.replace(punctuation, "");
+    newText = newText.replace(/\s+/g, '')
     return newText;
   }
   
 
 const palindromes = function(str) {
-    const cleaned = removePunctuation(str);
+    let cleaned = removePunctuation(str);
+    console.log(cleaned);
+    cleaned = cleaned.toLowerCase();
     const old = cleaned.split('');
-    //console.log(old);
+    console.log(old);
     let newArr = [];
-    //console.log(newArr);
+    console.log(newArr);
     for (let i = old.length -1; i >= 0; i--) {
         newArr.push(old[i]);
-        //console.log(newArr);
+        console.log(newArr);
     }
     let answer = newArr.join('');
-    //console.log(answer);
-    //console.log(str);
+    console.log(answer);
+    console.log(str);
     return answer === cleaned;
 };
 
-palindromes("racecar");
+palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
