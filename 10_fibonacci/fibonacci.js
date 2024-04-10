9b07b1fa32c5a332e0a0512210c5abdf753b64cf
SeanMero <MeroFromVero@gmail.com>
2024-04-10 15:43:08 +0800

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let answer = [];
+    if (n < 0) {
+        return "OOPS";
+    } else if (n >= 0) {
+        for (let i = 0; i <= n; i++) {
+            if (i === 0) {
+                answer.push(0)
+            } else if (i === 1) {
+                answer.push(1);
+            } else {
+                answer.push(answer[i-1] + answer[i-2])
+            }
+        };
+        return answer[n];
+    };
 };
 
 // Do not edit below this line
