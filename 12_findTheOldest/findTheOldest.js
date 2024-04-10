e646e1ccd2ec81aac2cb10e1ae1784eb806f4216
SeanMero <MeroFromVero@gmail.com>
2024-04-10 17:32:30 +0800

    Add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sorted = people.sort(function (a, b) {
+        let age1 = a.yearOfDeath - a.yearOfBirth;
+        let age2 = b.yearOfDeath - b.yearOfBirth;
+        const d = new Date;
+        //console.log(a.yearofDeath);
+        //console.log(b.yearofDeath);
+        if (a.yearOfDeath == undefined) {
+            age1 = d.getFullYear() - a.yearOfBirth;
+        };
+        if (b.yearOfDeath == undefined) {
+            age2 = d.getFullYear() - b.yearOfBirth;
+        };
+        //console.log(d.getFullYear());
+        //console.log(age1);
+        //console.log(age2);
+        return age2 - age1;
+    });
+    //console.table(sorted)
+    return sorted[0];
 };
+
+/* const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]; 
+
+findTheOldest(people); */
 
 // Do not edit below this line
 module.exports = findTheOldest;
